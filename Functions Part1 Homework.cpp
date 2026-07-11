9d3ab15efc23fadedc8ff3395ddae508235fe5ef
yousef-mohsen <UG_31201990@ics.tanta.edu.eg>
2026-07-11 03:27:50 +0300

    Add Functions Part 1 exercise

---
+++ Functions Part1 Homework.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+void DisplayMyCardInfoFunction() {
+
+	cout << "****************************************\n";
+	cout << "Name: Mohamed Abu-Hadhoud. " << endl;
+	cout << "Age: 44 Years" << endl;
+	cout << "City: Amman" << endl;
+	cout << "Coutnry: Jordan " << endl;
+	cout << "****************************************\n";
+}
+
+void PrintSquareStarsFunction() {
+
+	cout << "**********\n";
+	cout << "**********\n";
+	cout << "**********\n";
+	cout << "**********\n";
+}
+
+void PrintIloveProgrammingFunction() {
+
+	cout << "I Love Programming !" << endl;
+	cout << "I Promise To be the best Developer ever!" << endl;
+	cout << "I Know it will take some time to Practice,but I Will achieve my goal ISA" << endl;
+	cout << "Best Regards," << endl;
+	cout << "Mohamed Abu-Hadhoud" << endl;
+
+}
+
+void PrintHFuncion() {
+
+	cout << "*       *\n";
+	cout << "*       *\n";
+	cout << "*********\n";
+	cout << "*       *\n";
+	cout << "*       *\n";
+}
+
+int main() {
+
+	DisplayMyCardInfoFunction();
+	PrintSquareStarsFunction();
+	PrintIloveProgrammingFunction();
+	PrintHFuncion();
+
+	return 0;
+}
+
